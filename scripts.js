12afc64163843ca29fe0be48f2e3012644ff1af0
Md. Sakibul Islam Chy <79975417+Sakib-Chy@users.noreply.github.com>
2025-02-21 03:49:03 +0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,20 +1,14 @@
 document.addEventListener('DOMContentLoaded', function () {
     const sections = document.querySelectorAll('section');
 
-    sections.forEach(section => {
-        section.style.opacity = 0;
-        section.style.transform = 'translateY(50px)';
-    });
-
     window.addEventListener('scroll', () => {
         const scrollPosition = window.pageYOffset;
+        const windowHeight = window.innerHeight;
 
         sections.forEach(section => {
             const sectionPosition = section.offsetTop;
-            const windowHeight = window.innerHeight;
 
             if (scrollPosition + windowHeight > sectionPosition + 100) {
-                section.style.transition = 'opacity 0.5s, transform 0.5s';
                 section.style.opacity = 1;
                 section.style.transform = 'translateY(0)';
             }
